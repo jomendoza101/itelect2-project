e6b860eb08279f9732b6904d8c038d7303219783
Joenel Christian Mendoza <joenel_christian_mendoza@dlsl.edu.ph>
2026-07-03 16:02:53 +0800

    add: practice java file

---
+++ practice.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class practice {
+    public static void main(String [] args) {
+        Scanner hh = new Scanner(System.in); 
+
+        System.out.print("Enter name: ");
+        String name = hh.nextLine();
+
+        System.out.print("Hello! " + name);
+    }
+}
